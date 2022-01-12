47023094b3f71aa5772ec0dfab70ea882e8b84c5
Victor Lovera <victor260622@gmail.com>
2022-01-12 10:22:49 -0300

    Eliminando codigo inecesario

--- Back/db.js
+++ Back/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-/* .connect("mongodb://127.0.0.1:27017/TablaDePosiciones") */ 
+
 .connect(process.env.MONGODB)
 .then(() => console.log("Connected to MongoDB..."))
 .catch((err) => console.error("Could not connect to MongoDB..."));
